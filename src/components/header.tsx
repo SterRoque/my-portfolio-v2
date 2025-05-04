07f75ea94daf0d7f5e4277b64ed9b48a3132f9f0
Sterphane Roque <stermiss2@gmail.com>
2025-05-04 19:14:59 -0300

    feat: create Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+   return (
+      <header className='fixed z-30 flex h-20 w-full items-center justify-center'>
+         <nav className='flex gap-4 text-white'>
+            <a href=''>Home</a>
+            <a href=''>Projetos</a>
+            <a href=''>Contato</a>
+         </nav>
+      </header>
+   );
+}
