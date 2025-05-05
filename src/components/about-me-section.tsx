ead008dc67e999fe62ee83143675be92037bb524
Sterphane Roque <stermiss2@gmail.com>
2025-05-05 14:46:49 -0300

    feat: create AboutMeSection component

---
+++ src/components/about-me-section.tsx
@@ -0,0 +1,19 @@
+export function AboutMeSection() {
+   return (
+      <section className='flex h-fit flex-col items-center text-white'>
+         <h1 className='text-primary mb-10 text-center text-2xl font-semibold'>
+            Sobre mim
+         </h1>
+         <p className='mb-28 w-full text-justify'>
+            Sou Ster Roque, desenvolvedora full stack com 1 ano de experiência
+            na Varejo Chip e mais de 2 anos em projetos de desenvolvimento.
+            Tenho expertise em React, Next.js, Node.js, NestJS e Prisma, além de
+            experiência com React Native e atualmente estou explorando Kotlin.
+            Sou uma profissional curiosa e autodidata, com forte capacidade de
+            aprender novas tecnologias e paixão por resolver desafios através do
+            código. Estou em busca de oportunidades que me permitam crescer e
+            contribuir com soluções inovadoras.
+         </p>
+      </section>
+   );
+}
