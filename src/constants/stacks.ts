80198e6d9c5c34c972bddee1ae7d5710f52fecac
Sterphane Roque <stermiss2@gmail.com>
2025-05-05 14:46:14 -0300

    feat: add technology icons

---
+++ src/constants/stacks.ts
@@ -0,0 +1,77 @@
+export const stacks = [
+   {
+      stack: 'HTML',
+      icon: '/icons/html.png',
+   },
+
+   {
+      stack: 'CSS',
+      icon: '/icons/css.png',
+   },
+
+   {
+      stack: 'JavaScript',
+      icon: '/icons/javascript.png',
+   },
+
+   {
+      stack: 'TypeScript',
+      icon: '/icons/typescript.png',
+   },
+
+   {
+      stack: 'React.js',
+      icon: '/icons/react.png',
+   },
+
+   {
+      stack: 'React Native',
+      icon: '/icons/react-native.png',
+   },
+
+   {
+      stack: 'Node.js',
+      icon: '/icons/node.png',
+   },
+   {
+      stack: 'Fastify.js',
+      icon: '/icons/fastify.png',
+   },
+   {
+      stack: 'Express.js',
+      icon: '/icons/express.png',
+   },
+   {
+      stack: 'Nest.js',
+      icon: '/icons/nest.png',
+   },
+
+   {
+      stack: 'Astro.js',
+      icon: '/icons/astro.png',
+   },
+
+   {
+      stack: 'Angular.js',
+      icon: '/icons/angular.png',
+   },
+   {
+      stack: 'FiveM',
+      icon: '/icons/fivem.png',
+   },
+
+   {
+      stack: 'Tailwind',
+      icon: '/icons/tailwind.png',
+   },
+
+   {
+      stack: 'Prisma',
+      icon: '/icons/prisma.png',
+   },
+
+   {
+      stack: 'SQL',
+      icon: '/icons/sql.png',
+   },
+];
