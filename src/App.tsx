23c1cc4d7082ae9803cd5cc8660b0201e6b24117
Sterphane Roque <stermiss2@gmail.com>
2025-05-05 14:48:16 -0300

    feat: add AboutMeSection and StacksSection components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { AboutMeSection } from './components/about-me-section';
 import { HeroSection } from './components/hero-section';
 import { Layout } from './components/layout';
+import { StacksSection } from './components/stacks-section';
 
 export default function App() {
    return (
       <Layout>
-         <main className='h-screen w-screen px-44'>
+         <main className='h-screen md:px-44 xl:px-60'>
             <HeroSection />
+            <AboutMeSection />
+            <StacksSection />
          </main>
       </Layout>
    );
