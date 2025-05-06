0c4e589fa6fc6d6c3f77b70ea43468f99bdab81e
Sterphane Roque <stermiss2@gmail.com>
2025-05-06 02:06:11 -0300

    style: add margin to AboutMeSection

--- src/components/about-me-section.tsx
+++ src/components/about-me-section.tsx
@@ -1,10 +1,10 @@
 export function AboutMeSection() {
    return (
-      <section className='flex h-fit flex-col items-center text-white'>
+      <section className='my-32 flex h-fit flex-col items-center text-white'>
          <h1 className='text-primary mb-10 text-center text-2xl font-semibold'>
             Sobre mim
          </h1>
-         <p className='mb-28 w-full text-justify'>
+         <p className='w-full text-justify'>
             Sou Ster Roque, desenvolvedora full stack com 1 ano de experiência
             na Varejo Chip e mais de 2 anos em projetos de desenvolvimento.
             Tenho expertise em React, Next.js, Node.js, NestJS e Prisma, além de
