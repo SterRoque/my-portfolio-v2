69879c741b52120ce9780db76d8bd8954e742b48
Sterphane Roque <stermiss2@gmail.com>
2025-05-06 02:04:02 -0300

    feat: create IProject interface

---
+++ src/interfaces/project-interface.ts
@@ -0,0 +1,14 @@
+export interface IProject {
+   id: string;
+   img: string;
+   imgScreens?: string[];
+   title: string;
+   description: string;
+   descriptionDetails: string;
+   technologies: string[];
+   features: string[];
+   conclusion: string;
+   urlSite?: string;
+   urlGithub?: string;
+   stacks: string[];
+}
