c6519d448c3c34667988bf24553497d863c0203e
Sterphane Roque <stermiss2@gmail.com>
2025-05-06 02:09:03 -0300

    feat: add ProjectsSection component to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { AboutMeSection } from './components/about-me-section';
 import { HeroSection } from './components/hero-section';
 import { Layout } from './components/layout';
+import { ProjectsSection } from './components/projects-section';
 import { StacksSection } from './components/stacks-section';
 
 export default function App() {
    return (
       <Layout>
-         <main className='h-screen md:px-44 xl:px-60'>
+         <main className='h-screen md:px-32 2xl:px-60'>
             <HeroSection />
             <AboutMeSection />
             <StacksSection />
+            <ProjectsSection />
          </main>
       </Layout>
    );
